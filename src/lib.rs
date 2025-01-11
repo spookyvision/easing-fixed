07fadb609027c1f3ae78167c46529b8723e11176
Anatol Ulrich <e+github@mail.taugt.net>
2025-01-11 18:45:11 +0100

    do the actual port to `fixed`; change exp_out to be less overflowy; change tests to error margin check instead of equality and generate json data for failing tests; remove vendored fixed-exp dir and use git fork instead

--- src/lib.rs
+++ src/lib.rs
@@ -1,284 +1,289 @@
 // Copyright 2016 John Ward under MIT
 
-use std::f64::consts::{FRAC_PI_2};
+use std::f64::consts::FRAC_PI_2;
+
+use fixed::types::I16F16;
+use fixed_exp::FixedPowF;
+use fixed_trigonometry::sin;
+pub(crate) type Fix = I16F16;
 
 macro_rules! easer {
-    ($f:ident, $t:ident, $e:expr) => (
+    ($f:ident, $t:ident, $e:expr) => {
         pub struct $t {
-            start: f64,
-            dist: f64,
+            start: Fix,
+            dist: Fix,
             step: u64,
             steps: u64,
         }
 
-        pub fn $f(start: f64, end: f64, steps: u64) -> $t {
+        pub fn $f(start: Fix, end: Fix, steps: u64) -> $t {
             $t {
-                start: start,
+                start,
                 dist: end - start,
                 step: 0,
-                steps: steps,
+                steps,
             }
         }
 
         impl Iterator for $t {
-            type Item = f64;
+            type Item = Fix;
 
-            fn next(&mut self) -> Option<f64> {
+            fn next(&mut self) -> Option<Fix> {
                 self.step += 1;
                 if self.step > self.steps {
                     None
                 } else {
-                    let x = self.step as f64 / self.steps as f64;
-                    Some($e(x).mul_add(self.dist, self.start))
+                    let x: Fix = Fix::from_num(self.step) / Fix::from_num(self.steps);
+                    Some(Fix::from_num($e(x)).mul_add(self.dist, self.start))
                 }
             }
         }
-    )
+    };
 }
 
-easer!(linear, Linear, |x:f64| {
-    x
+easer!(linear, Linear, |x: Fix| { x });
+easer!(quad_in, QuadIn, |x: Fix| { x * x });
+easer!(quad_out, QuadOut, |x: Fix| {
+    -(x * (x - Fix::from_num(2)))
 });
-easer!(quad_in, QuadIn, |x:f64| {
-    x * x
-});
-easer!(quad_out, QuadOut, |x:f64| {
-    -(x * (x - 2.))
-});
-easer!(quad_inout, QuadInOut, |x:f64| {
-    if x < 0.5 { 2. * x * x }
-    else { (-2. * x * x) + x.mul_add(4., -1.) }
-});
-easer!(cubic_in, CubicIn, |x:f64| {
-    x * x * x
-});
-easer!(cubic_out, CubicOut, |x:f64| {
-    let y = x - 1.;
-    y * y * y + 1.
-});
-easer!(cubic_inout, CubicInOut, |x:f64| {
-    if x < 0.5 { 4. * x * x * x }
-    else {
-        let y = x.mul_add(2., -2.);
-        (y * y * y).mul_add(0.5, 1.)
+easer!(quad_inout, QuadInOut, |x: Fix| -> Fix {
+    if x < Fix::from_num(0.5) {
+        Fix::from_num(2) * x * x
+    } else {
+        (Fix::from_num(-2) * x * x) + x.mul_add(Fix::from_num(4), Fix::from_num(-1))
     }
 });
-easer!(quartic_in, QuarticIn, |x:f64| {
-    x * x * x * x
+easer!(cubic_in, CubicIn, |x: Fix| { x * x * x });
+easer!(cubic_out, CubicOut, |x: Fix| {
+    let y = x - Fix::from_num(1);
+    y * y * y + Fix::from_num(1)
 });
-easer!(quartic_out, QuarticOut, |x:f64| {
-    let y = x - 1.;
-    (y * y * y).mul_add(1. - x, 1.)
-});
-easer!(quartic_inout, QuarticInOut, |x:f64| {
-    if x < 0.5 { 8. * x * x * x * x }
-    else {
-        let y = x - 1.;
-        (y * y * y * y).mul_add(-8., 1.)
+easer!(cubic_inout, CubicInOut, |x: Fix| {
+    if x < Fix::from_num(0.5) {
+        Fix::from_num(4) * x * x * x
+    } else {
+        let y = x.mul_add(2.into(), Fix::from_num(-2));
+        (y * y * y).mul_add(Fix::from_num(0.5), Fix::from_num(1))
     }
 });
-easer!(sin_in, SinIn, |x:f64| {
-    let y = (x - 1.) * FRAC_PI_2;
-    y.sin() + 1.
+easer!(quartic_in, QuarticIn, |x: Fix| { x * x * x * x });
+easer!(quartic_out, QuarticOut, |x: Fix| {
+    let y = x - Fix::from_num(1);
+    (y * y * y).mul_add(Fix::from_num(1) - x, Fix::from_num(1))
 });
-easer!(sin_out, SinOut, |x:f64| {
-    (x * FRAC_PI_2).sin()
+easer!(quartic_inout, QuarticInOut, |x: Fix| {
+    if x < Fix::from_num(0.5) {
+        Fix::from_num(8) * x * x * x * x
+    } else {
+        let y = x - Fix::from_num(1);
+        (y * y * y * y).mul_add(Fix::from_num(-8), Fix::from_num(1))
+    }
 });
-easer!(sin_inout, SinInOut, |x:f64| {
-    if x < 0.5 { 0.5 * (1. - (x * x).mul_add(-4., 1.).sqrt()) }
-    else       { 0.5 * ((x.mul_add(-2., 3.) * x.mul_add(2., -1.)).sqrt() + 1.) }
+easer!(sin_in, SinIn, |x: Fix| {
+    let y = (x - Fix::from_num(1)) * Fix::from_num(FRAC_PI_2);
+    sin(y) + Fix::from_num(1)
 });
-easer!(exp_in, ExpIn, |x:f64| {
-    if x == 0. { 0. }
-    else       { (10. * (x - 1.)).exp2() }
+easer!(sin_out, SinOut, |x: Fix| {
+    sin(x * Fix::from_num(FRAC_PI_2))
 });
-easer!(exp_out, ExpOut, |x:f64| {
-    if x == 1. { 1. }
-    else       { 1. - (-10. * x).exp2() }
+easer!(sin_inout, SinInOut, |x: Fix| {
+    if x < Fix::from_num(0.5) {
+        Fix::from_num(0.5)
+            * (Fix::from_num(1) - (x * x).mul_add(Fix::from_num(-4), Fix::from_num(1)).sqrt())
+    } else {
+        Fix::from_num(0.5)
+            * ((x.mul_add(Fix::from_num(-2), Fix::from_num(3))
+                * x.mul_add(Fix::from_num(2), Fix::from_num(-1)))
+            .sqrt()
+                + Fix::from_num(1))
+    }
 });
-easer!(exp_inout, ExpInOut, |x:f64| {
-    if      x == 1. { 1. }
-    else if x == 0. { 0. }
-    else if x < 0.5 { x.mul_add(20., -10.).exp2() * 0.5 }
-    else            { x.mul_add(-20., 10.).exp2().mul_add(-0.5, 1.) }
+easer!(exp_in, ExpIn, |x: Fix| {
+    if x == 0. {
+        Fix::from_num(0)
+    } else {
+        Fix::from_num(2).powf(Fix::from_num(10) * (x - Fix::from_num(1)))
+    }
+});
+
+easer!(exp_out, ExpOut, |x: Fix| {
+    if x == Fix::from_num(1) {
+        Fix::from_num(1)
+    } else {
+        Fix::from_num(2).powf(-Fix::from_num(10) * x) * Fix::from_num(-1) + Fix::from_num(1)
+    }
+});
+easer!(exp_inout, ExpInOut, |x: Fix| {
+    if x == Fix::from_num(1) {
+        Fix::from_num(1)
+    } else if x == 0. {
+        Fix::from_num(0)
+    } else if x < Fix::from_num(0.5) {
+        Fix::from_num(2).powf(x.mul_add(Fix::from_num(20), Fix::from_num(-10))) * Fix::from_num(0.5)
+    } else {
+        Fix::from_num(2)
+            .powf(x.mul_add(Fix::from_num(-20), Fix::from_num(10)))
+            .mul_add(Fix::from_num(-0.5), Fix::from_num(1))
+    }
 });
 
 #[cfg(test)]
 mod test {
+    // acceptable relative error (0.1%)
+    const ERROR_MARGIN_FAC: f64 = 0.00015;
+
+    use std::{fs::File, iter::zip, path::PathBuf};
+
+    use anyhow::anyhow;
+
     use super::*;
+    macro_rules! function {
+        () => {{
+            fn f() {}
+            fn type_name_of<T>(_: T) -> &'static str {
+                std::any::type_name::<T>()
+            }
+            let name = type_name_of(f);
+            let full_path = name.strip_suffix("::f").unwrap();
+            full_path.split("::").last().unwrap()
+        }};
+    }
 
-    // Rounds a float to five places after the decimal.
-    // We can't use the exact value because of floating point
-    // problems (13 != 12.999999999999999), and five decimal
-    // points of precision is fine for this.
-    fn round_5(x: f64) -> f64 {
-        (x * 10E+5).round() / 10E+5
+    /// accept a Vec of `is_data` if it is within the defined error margin of `ought_data`
+    fn must_be_withing_error_margin_else_write(
+        test_name: &str,
+        ought_data: Vec<f64>,
+        is_data: Vec<Fix>,
+    ) -> anyhow::Result<()> {
+        let min = ought_data
+            .iter()
+            .min_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap();
+        let max = ought_data
+            .iter()
+            .max_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap();
+        let value_range = max - min;
+
+        let max_error = (value_range * ERROR_MARGIN_FAC).abs();
+        let inside_margin = |is: f64, ought: f64| -> bool {
+            let delta = (is - ought).abs();
+
+            let res = delta <= max_error;
+            if !res {
+                eprintln!("measured error outside of acceptable margin: {is} <> {ought}");
+            }
+            res
+        };
+
+        let mut ok = true;
+        for (ought, is) in zip(ought_data.iter(), is_data.iter()) {
+            let is = is.to_num::<f64>();
+            if !inside_margin(is, *ought) {
+                ok = false;
+            }
+        }
+
+        if !ok {
+            let root = option_env!("CARGO_MANIFEST_DIR")
+                .ok_or(anyhow!("missing env var CARGO_MANIFEST_DIR"))?;
+            let root = PathBuf::from(root);
+            let target_path = root.join("jupyter-tests");
+
+            let ought_file = File::create(target_path.join(format!("{test_name}-ought.json")))?;
+            let is_file = File::create(target_path.join(format!("{test_name}-is.json")))?;
+
+            let is_converted = is_data
+                .into_iter()
+                .map(|fix| fix.to_num())
+                .collect::<Vec<f64>>();
+            serde_json::to_writer(ought_file, &ought_data)?;
+            serde_json::to_writer(is_file, &is_converted)?;
+
+            panic!("{test_name} outside of error {ERROR_MARGIN_FAC} margin: {is_converted:?} <> {ought_data:?}");
+        }
+        Ok(())
     }
 
     #[test]
-    fn linear_test() {
+    fn linear_test() -> anyhow::Result<()> {
+        let model = vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
+        let res: Vec<Fix> = linear(Fix::from_num(0), Fix::from_num(1), 10).collect();
+        must_be_withing_error_margin_else_write(function!(), model, res)
+    }
+
+    #[test]
+    fn quad_in_test() -> anyhow::Result<()> {
         let model = vec![
-            0.1,
-            0.2,
-            0.3,
-            0.4,
-            0.5,
-            0.6,
-            0.7,
-            0.8,
-            0.9,
-            1.0,
+            100., 400., 900., 1600., 2500., 3600., 4900., 6400., 8100., 10000.,
         ];
-        let try: Vec<f64> = linear(0f64, 1f64, 10).collect();
-        assert_eq!(try, model);
+        let res: Vec<Fix> = quad_in(Fix::from_num(0), Fix::from_num(10000), 10).collect();
+        must_be_withing_error_margin_else_write(function!(), model, res)
     }
 
     #[test]
-    fn quad_in_test() {
+    fn quad_out_test() -> anyhow::Result<()> {
         let model = vec![
-            100.,
-            400.,
-            900.,
-            1600.,
-            2500.,
-            3600.,
-            4900.,
-            6400.,
-            8100.,
-            10000.,
+            1900., 3600., 5100., 6400., 7500., 8400., 9100., 9600., 9900., 10000.,
         ];
-        let try: Vec<f64> = quad_in(0f64, 10000f64, 10).map(round_5).collect();
-        assert_eq!(try, model);
+        let res: Vec<Fix> = quad_out(Fix::from_num(0), Fix::from_num(10000), 10).collect();
+        must_be_withing_error_margin_else_write(function!(), model, res)
     }
 
     #[test]
-    fn quad_out_test() {
+    fn quad_inout_test() -> anyhow::Result<()> {
         let model = vec![
-            1900.,
-            3600.,
-            5100.,
-            6400.,
-            7500.,
-            8400.,
-            9100.,
-            9600.,
-            9900.,
-            10000.,
+            200., 800., 1800., 3200., 5000., 6800., 8200., 9200., 9800., 10000.,
         ];
-        let try: Vec<f64> = quad_out(0f64, 10000f64, 10).map(round_5).collect();
-        assert_eq!(try, model);
+        let res: Vec<Fix> = quad_inout(Fix::from_num(0), Fix::from_num(10000), 10).collect();
+        must_be_withing_error_margin_else_write(function!(), model, res)
     }
 
     #[test]
-    fn quad_inout_test() {
+    fn cubic_in_test() -> anyhow::Result<()> {
         let model = vec![
-            200.,
-            800.,
-            1800.,
-            3200.,
-            5000.,
-            6800.,
-            8200.,
-            9200.,
-            9800.,
-            10000.,
+            10., 80., 270., 640., 1250., 2160., 3430., 5120., 7290., 10000.,
         ];
-        let try: Vec<f64> = quad_inout(0f64, 10000f64, 10).map(round_5).collect();
-        assert_eq!(try, model);
+        let res: Vec<Fix> = cubic_in(Fix::from_num(0), Fix::from_num(10000), 10).collect();
+        must_be_withing_error_margin_else_write(function!(), model, res)
     }
 
     #[test]
-    fn cubic_in_test() {
+    fn cubic_out_test() -> anyhow::Result<()> {
         let model = vec![
-            10.,
-            80.,
-            270.,
-            640.,
-            1250.,
-            2160.,
-            3430.,
-            5120.,
-            7290.,
-            10000.,
+            2710., 4880., 6570., 7840., 8750., 9360., 9730., 9920., 9990., 10000.,
         ];
-        let try: Vec<f64> = cubic_in(0f64, 10000f64, 10).map(round_5).collect();
-        assert_eq!(try, model);
+        let res: Vec<Fix> = cubic_out(Fix::from_num(0), Fix::from_num(10000), 10).collect();
+        must_be_withing_error_margin_else_write(function!(), model, res)
     }
 
     #[test]
-    fn cubic_out_test() {
+    fn quartic_in_test() -> anyhow::Result<()> {
+        let model = vec![1., 16., 81., 256., 625., 1296., 2401., 4096., 6561., 10000.];
+        let res: Vec<Fix> = quartic_in(Fix::from_num(0), Fix::from_num(10000), 10).collect();
+        must_be_withing_error_margin_else_write(function!(), model, res)
+    }
+
+    #[test]
+    fn quartic_out_test() -> anyhow::Result<()> {
         let model = vec![
-            2710.,
-            4880.,
-            6570.,
-            7840.,
-            8750.,
-            9360.,
-            9730.,
-            9920.,
-            9990.,
-            10000.,
+            3439., 5904., 7599., 8704., 9375., 9744., 9919., 9984., 9999., 10000.,
         ];
-        let try: Vec<f64> = cubic_out(0f64, 10000f64, 10).map(round_5).collect();
-        assert_eq!(try, model);
+        let res: Vec<Fix> = quartic_out(Fix::from_num(0), Fix::from_num(10000), 10).collect();
+        must_be_withing_error_margin_else_write(function!(), model, res)
     }
 
     #[test]
-    fn quartic_in_test() {
+    fn quartic_inout_test() -> anyhow::Result<()> {
         let model = vec![
-            1.,
-            16.,
-            81.,
-            256.,
-            625.,
-            1296.,
-            2401.,
-            4096.,
-            6561.,
-            10000.,
+            8., 128., 648., 2048., 5000., 7952., 9352., 9872., 9992., 10000.,
         ];
-        let try: Vec<f64> = quartic_in(0f64, 10000f64, 10).map(round_5).collect();
-        assert_eq!(try, model);
+        let res: Vec<Fix> = quartic_inout(Fix::from_num(0), Fix::from_num(10000), 10).collect();
+        must_be_withing_error_margin_else_write(function!(), model, res)
     }
 
     #[test]
-    fn quartic_out_test() {
-        let model = vec![
-            3439.,
-            5904.,
-            7599.,
-            8704.,
-            9375.,
-            9744.,
-            9919.,
-            9984.,
-            9999.,
-            10000.,
-        ];
-        let try: Vec<f64> = quartic_out(0f64, 10000f64, 10).map(round_5).collect();
-        assert_eq!(try, model);
-    }
-
-    #[test]
-    fn quartic_inout_test() {
-        let model = vec![
-            8.,
-            128.,
-            648.,
-            2048.,
-            5000.,
-            7952.,
-            9352.,
-            9872.,
-            9992.,
-            10000.,
-        ];
-        let try: Vec<f64> = quartic_inout(0f64, 10000f64, 10).map(round_5).collect();
-        assert_eq!(try, model);
-    }
-
-    #[test]
-    fn sin_in_test() {
+    fn sin_in_test() -> anyhow::Result<()> {
         let model = vec![
             123.116594,
             489.434837,
@@ -291,12 +296,12 @@ mod test {
             8435.655350,
             10000.,
         ];
-        let try: Vec<f64> = sin_in(0f64, 10000f64, 10).map(round_5).collect();
-        assert_eq!(try, model);
+        let res: Vec<Fix> = sin_in(Fix::from_num(0), Fix::from_num(10000), 10).collect();
+        must_be_withing_error_margin_else_write(function!(), model, res)
     }
 
     #[test]
-    fn sin_out_test() {
+    fn sin_out_test() -> anyhow::Result<()> {
         let model = vec![
             1564.344650,
             3090.169944,
@@ -307,14 +312,14 @@ mod test {
             8910.065242,
             9510.565163,
             9876.883406,
-            10000.
+            10000.,
         ];
-        let try: Vec<f64> = sin_out(0f64, 10000f64, 10).map(round_5).collect();
-        assert_eq!(try, model);
+        let res: Vec<Fix> = sin_out(Fix::from_num(0), Fix::from_num(10000), 10).collect();
+        must_be_withing_error_margin_else_write(function!(), model, res)
     }
 
     #[test]
-    fn sin_inout_test() {
+    fn sin_inout_test() -> anyhow::Result<()> {
         let model = vec![
             101.020514,
             417.424305,
@@ -325,63 +330,36 @@ mod test {
             9000.,
             9582.575695,
             9898.979486,
-            10000.
+            10000.,
         ];
-        let try: Vec<f64> = sin_inout(0f64, 10000f64, 10).map(round_5).collect();
-        assert_eq!(try, model);
+        let res: Vec<Fix> = sin_inout(Fix::from_num(0), Fix::from_num(10000), 10).collect();
+        must_be_withing_error_margin_else_write(function!(), model, res)
     }
 
     #[test]
-    fn exp_in_test() {
+    fn exp_in_test() -> anyhow::Result<()> {
         let model = vec![
-            19.53125,
-            39.0625,
-            78.125,
-            156.25,
-            312.5,
-            625.,
-            1250.,
-            2500.,
-            5000.,
-            10000.
+            19.53125, 39.0625, 78.125, 156.25, 312.5, 625., 1250., 2500., 5000., 10000.,
         ];
-        let try: Vec<f64> = exp_in(0f64, 10000f64, 10).map(round_5).collect();
-        assert_eq!(try, model);
+        let res: Vec<Fix> = exp_in(Fix::from_num(0), Fix::from_num(10000), 10).collect();
+        must_be_withing_error_margin_else_write(function!(), model, res)
     }
 
     #[test]
-    fn exp_out_test() {
+    fn exp_out_test() -> anyhow::Result<()> {
         let model = vec![
-            5000.,
-            7500.,
-            8750.,
-            9375.,
-            9687.5,
-            9843.75,
-            9921.875,
-            9960.9375,
-            9980.46875,
-            10000.
+            5000., 7500., 8750., 9375., 9687.5, 9843.75, 9921.875, 9960.9375, 9980.46875, 10000.,
         ];
-        let try: Vec<f64> = exp_out(0f64, 10000f64, 10).map(round_5).collect();
-        assert_eq!(try, model);
+        let res: Vec<Fix> = exp_out(Fix::from_num(0), Fix::from_num(10000), 10).collect();
+        must_be_withing_error_margin_else_write(function!(), model, res)
     }
 
     #[test]
-    fn exp_inout_test() {
+    fn exp_inout_test() -> anyhow::Result<()> {
         let model = vec![
-            19.53125,
-            78.125,
-            312.5,
-            1250.,
-            5000.,
-            8750.,
-            9687.5,
-            9921.875,
-            9980.46875,
-            10000.
+            19.53125, 78.125, 312.5, 1250., 5000., 8750., 9687.5, 9921.875, 9980.46875, 10000.,
         ];
-        let try: Vec<f64> = exp_inout(0f64, 10000f64, 10).map(round_5).collect();
-        assert_eq!(try, model);
+        let res: Vec<Fix> = exp_inout(Fix::from_num(0), Fix::from_num(10000), 10).collect();
+        must_be_withing_error_margin_else_write(function!(), model, res)
     }
 }
